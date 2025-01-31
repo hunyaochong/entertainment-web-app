85aa044628ce9b380196cc38fac73057f8fddc27
ZephyRemix <hunyaochong@gmail.com>
2025-01-31 11:18:51 +0800

    Implemented sidenav component

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,13 +1,51 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./SideNav.module.css";
+
 import logo from "@/assets/logo.svg";
+import homeLogo from "@/assets/icon-nav-home.svg";
+import moviesLogo from "@/assets/icon-nav-movies.svg";
+import tvSeriesLogo from "@/assets/icon-nav-tv-series.svg";
+import bookmarkLogo from "@/assets/icon-nav-bookmark.svg";
+import avatarImage from "@/assets/image-avatar.png";
 
 function SideNav() {
   return (
-    <nav>
-      <a href="/">
-        <img src={logo} alt="Entertainment app logo" />
-      </a>
+    <nav className={styles.sidenav}>
+      <Image
+        src={logo}
+        alt="Entertainment app logo"
+        width={32}
+        height={25}
+        className={styles.logo}
+      />
+
+      <div className={styles.nav_btn}>
+        <a href="/">
+          <Image src={homeLogo} alt="Link to home page"></Image>
+        </a>
+        <a href="/movies">
+          <Image src={moviesLogo} alt="Link to movies index page"></Image>
+        </a>
+        <a href="/tv-series">
+          <Image src={tvSeriesLogo} alt="Link to TV series page"></Image>
+        </a>
+        <a href="bookmarks">
+          <Image
+            src={bookmarkLogo}
+            alt="Link to bookmarked movies page"
+          ></Image>
+        </a>
+      </div>
+
+      <div className={styles.avatarWrapper}>
+        <Image
+          src={avatarImage}
+          alt="User profile picture"
+          width={40}
+          height={40}
+        />
+      </div>
     </nav>
   );
 }
